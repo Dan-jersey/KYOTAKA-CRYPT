2a0ddf468ea563cd99540286b96bb5b325db0799
ᏦᎽᎾᎿᎯᏦᎯ <stromjersey@gmail.com>
2025-07-24 00:01:51 +0200

    Update kyotaka_offusque.sh

--- kyotaka_offusque.sh
+++ kyotaka_offusque.sh
@@ -3,34 +3,36 @@
 clear
 echo -e "\033[1;31m"
 echo "╔════════════════════════════════════╗"
-echo "║        𝙺𝚈𝙾𝚃𝙰𝙺𝙰 𝙲𝚁𝚈𝙿𝚃 𝙼𝙾𝙳𝙴        ║"
+echo "║        𝙺𝚈𝙾𝚃𝙰𝙺𝙰 𝙲𝚁𝚈𝙿𝚃 𝙲𝙾𝙳𝙴       ║"
 echo "╚════════════════════════════════════╝"
 echo -e "\033[0m"
 
-read -p $'\033[1;36mMode ? (offusque/desoffusque) > \033[0m' mode
-[[ "$mode" != "offusque" && "$mode" != "desoffusque" ]] && echo -e "\033[1;31mInvalide.\033[0m" && exit 1
+read -p $'\033[1;36m(offusque / desoffusque) > \033[0m' mode
+[[ "$mode" != "offusque" && "$mode" != "desoffusque" ]] && { echo -e "\033[1;31mInvalide.\033[0m"; exit 1; }
 
-read -p $'\033[1;36mType (js/css/html) > \033[0m' type
-[[ "$type" != "js" && "$type" != "css" && "$type" != "html" ]] && echo -e "\033[1;31mType invalide.\033[0m" && exit 1
+read -p $'\033[1;36m(js / css / html) > \033[0m' type
+[[ "$type" != "js" && "$type" != "css" && "$type" != "html" ]] && { echo -e "\033[1;31mInvalide.\033[0m"; exit 1; }
 
-echo -e "\033[1;33mColle ton code, termine avec une ligne contenant uniquement : EOF\033[0m"
+echo -e "\033[1;33mColle ton code puis appuie deux fois sur Entrée :\033[0m"
 
 code=""
 while IFS= read -r line; do
-  [[ "$line" == "EOF" ]] && break
+  [[ -z "$line" ]] && break
   code+="$line"$'\n'
 done
 
-mkdir -p /sdcard/KYOTAKA
-filename="kyotaka_${mode}.${type}"
-filepath="/sdcard/KYOTAKA/$filename"
+filename="/sdcard/kyotaka_code_${mode}.${type}"
 
 if [[ "$mode" == "offusque" ]]; then
-  minified=$(echo "$code" | tr -d '\n\r\t' | sed 's/  */ /g' | sed 's/ *{ */{/g' | sed 's/ *} */}/g' | sed 's/ *; */;/g' | sed 's/ *, */,/g')
-  echo "$minified" | base64 > "$filepath"
+  if [[ "$type" == "html" ]]; then
+    echo "$code" | base64 > "$filename"
+  else
+    minified=$(echo "$code" | tr -d '\n\r\t' | sed 's/  */ /g' | sed 's/ *{ */{/g' | sed 's/ *} */}/g' | sed 's/ *; */;/g' | sed 's/ *, */,/g')
+    echo "$minified" | base64 > "$filename"
+  fi
 else
-  base64 -d <<< "$code" | fold -w 80 > "$filepath"
+  base64 -d "$filename" > "/sdcard/kyotaka_decoded.${type}"
 fi
 
-echo -e "\033[1;32mCode $mode avec succès !\033[0m"
-echo -e "\033[1;32mFichier créé : $filepath\033[0m"
+echo -e "\033[1;32mCode $mode avec succès.\033[0m"
+echo -e "\033[1;32mFichier : /sdcard/kyotaka_code_${mode}.${type}\033[0m"
